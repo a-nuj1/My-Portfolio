614c90adc1f45a4cc14a0dbb41b93d5a402ddd82
Anuj <anujgupta1532003@gmail.com>
2025-05-11 22:20:05 +0530

    new portfolio done

---
+++ src/Components/Data/data.js
@@ -0,0 +1,82 @@
+
+
+export const projects = {
+    allProject: [
+        {
+            image: 'https://res.cloudinary.com/dvdfhripy/image/upload/v1743508008/Screenshot_2025-03-26_201422_ke4fct.png',
+            title: 'CodeCompete | Under Development',
+            description: 'Developing an interactive coding competition platform that allows users to participate in topic-specific contests,create private groups, and track their rankings locally and globally. The system ensures fairness with randomly assigned questions and flexible contest timing', 
+            sourcecode: 'https://github.com/a-nuj1/CP-Application-Frontend',
+            livelink: 'https://cp-application-frontend.vercel.app/',
+            teckstack: ["React","Tailwind", " Material-UI", "Node.js", "Express.js", "MongoDB", "Socket.io"],
+            status: 'live', 
+            featured: true 
+
+        },
+        {
+            image: 'https://plus.unsplash.com/premium_photo-1721955487786-76802cbf0812?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            title: 'BaatCheet-Chatspace | LIVE Site available',
+            description: 'Developed Baatcheet - ChatSpace, a full-stack web application that facilitates real-time messaging between users. Implemented features such as user search, group creation and management, and personalized profiles with bios and avatars, enhancing user interaction and community building.',
+            sourcecode: 'https://github.com/a-nuj1/BaatCheet-ChatSpace',
+            livelink: 'https://baat-cheet-chat-space.vercel.app/',
+            teckstack: ["JavaScript","React", "Material UI", "Redux" ,"Node.js", "Express.js", "MongoDB", "Socket.io"],
+            status: 'live', 
+            featured: true 
+        },
+        {
+            image:'https://github.com/a-nuj1/Git-Commit-Analyzer/blob/main/image.png?raw=true',
+            title: 'Git-Commit-Analyzer | LIVE site available',
+            description: 'Developed Git-Commit-Analyzer, a web application that analyzes Git commit messages to provide insights into project activity and developer contributions. The app visualizes commit data, helping teams understand their development patterns and improve collaboration.',
+            sourcecode: 'https://github.com/a-nuj1/Git-Commit-Analyzer',
+            livelink: 'https://git-commit-analyzer-yso9.vercel.app/',
+            teckstack: ["TypeScript", "Vite", "TailwindCSS"],
+            status: 'live', 
+            featured: true 
+        },
+        {
+
+            image: 'https://res.cloudinary.com/dvdfhripy/image/upload/v1731412801/Screenshot_2024-11-12_124136_h5cubs.png',
+            title: 'Photo Storage | LIVE site available',
+            description: 'Developed PhotoStorage, a social photo storage app allowing users to securely upload, store, and browse images. Implemented real-time uploads, organized galleries, and social features with an intuitive, responsive interface for seamless browsing on desktop and mobile.',
+            sourcecode: 'https://github.com/a-nuj1/photo_storage',
+            livelink: 'https://photo-storage-azure.vercel.app/',
+            teckstack: ["React", "FireBase", "Tailwind CSS"],
+            status: 'live', 
+            featured: true 
+        },
+        {
+            image: 'https://res.cloudinary.com/dvdfhripy/image/upload/v1731412795/Screenshot_2024-11-12_101453_okigjh.png',
+            title: 'Student-Compass | Live Site N/A',
+            description: 'Developed a fullstack student-centric learning platform using MERN stack to help students track their learning progress and manage all course materials in onecentralized location',
+            sourcecode: 'https://github.com/a-nuj1/student_compass',
+            livelink: 'https://github.com/a-nuj1/page-not-found',
+            teckstack: ["JavaScript", "React","Redux","Chakra UI","Node.js","Express.js","MongoDB"],
+            // status: 'live', 
+            // featured: true 
+        },
+        {
+            image: 'https://res.cloudinary.com/dvdfhripy/image/upload/v1731412796/Screenshot_2024-11-12_172310_xrbqeh.png',
+            title: 'Write-Space | LIVE Site available',
+            description: 'Developed Write-Space, a dynamic blog application featuring secure user authentication, rich text editing, and comprehensive post management. Enabled users to easily create, edit, and manage formatted posts with titles, images, and intuitive navigation',
+            sourcecode: 'https://github.com/a-nuj1/daily_article',
+            livelink: 'https://daily-article.vercel.app/',
+            teckstack: ["React", "Tailwind CSS", "Node.js", "Express.js", "Appwrite"]
+        },
+        {
+            image: 'https://res.cloudinary.com/dvdfhripy/image/upload/v1731412803/Screenshot_2024-11-12_131140_whwlu2.png',
+            title: 'Movie World | LIVE site available',
+            description: 'A simple React Page that fetch movies details from API and show it on card, users can search movies too.',
+            sourcecode: 'https://github.com/a-nuj1/netflix_clone',
+            livelink: 'https://netflix-clone-nu-henna.vercel.app/',
+            teckstack: ["ReactJs", "REST API", "Axios"]
+        },
+        {
+            image: 'https://static.vecteezy.com/system/resources/thumbnails/007/343/533/small/yellow-quiz-time-banner-with-comic-style-background-suitable-for-use-for-promotional-designs-vector.jpg',
+            title: 'Quiz-Time| LIVE',
+            description: 'Develop a dynamic quiz application that supports various question types, offering an interactive experience with real-time scoring and feedback. The goal was to create a versatile and user-friendly platform for assessments and learning.',
+            sourcecode: 'https://github.com/a-nuj1/quizFrontend',
+            livelink: 'https://quiz-frontend-mauve.vercel.app/',
+            teckstack: ["ReactJs", "REST API", "Node.js", "Express.js", "MongoDB"]
+        },
+    ],
+}
