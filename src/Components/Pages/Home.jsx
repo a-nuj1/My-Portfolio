9c3e0153466b4c5759443c8c8f72217b6b1e12f7
Anuj Gupta <anuj@inquisitivedigital.com>
2026-05-11 19:37:44 +0530

    resume updated

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -127,7 +127,7 @@ const Home = () => {
 
           <div className="flex gap-4 justify-center md:justify-start">
             <motion.a
-              href="https://drive.google.com/file/d/1F8XeYfV8lX7AqbEWCHRGjDGZmFyW8d9c/view?usp=sharing"
+              href="https://drive.google.com/file/d/17EEmBVvDudD5prL5pm18nxS849H2GDJ8/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ scale: 1.05 }}
