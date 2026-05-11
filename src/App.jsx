542869319fe5faf111a802a7bd4f4c4bafa38b5b
Anuj Gupta <anujgupta1532003@gmail.com>
2026-05-11 19:54:08 +0530

    checking if workign good

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Skills from './Components/Pages/Skills.jsx';
 import Projects from './Components/Pages/Projects.jsx';
 import Footer from './Components/Pages/Footer.jsx';
 
-
+// main function
 function App() {
   return (
     <div className="min-h-screen w-full overflow-x-hidden relative">      
